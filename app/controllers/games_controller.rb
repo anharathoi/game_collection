3b487d02272e0200dfa1bce0fe7f82216096a561
Anhar Athoi <anharzihan@gmail.com>
2018-12-05 16:10:22 +1100

    Added s3 bucket for images

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -75,6 +75,6 @@ class GamesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def game_params
-      params.require(:game).permit(:title, :year, :description)
+      params.require(:game).permit(:title, :year, :description, :image)
     end
 end
